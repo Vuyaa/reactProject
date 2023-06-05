17d95dc2fe95bfe970c88c1eade02d64c357e998
Vuyaa <st_im@hotmail.com>
2023-06-05 17:44:00 +0200

    Added Filter functionalities

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -5,18 +5,8 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEneteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-  };
-
-  const amountChangeHandler = (event) => {
-    setEneteredAmount(event.target.value);
-  };
-
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
-  };
+  const [formExpanded, setFormExpanded] = useState(false);
+  const [buttonHidden, setButtonHidden] = useState(true);
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -31,46 +21,79 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredDate("");
     setEneteredAmount("");
+
+    closeFormHandler();
+  };
+
+  const closeFormHandler = () => {
+    setFormExpanded(false);
+    setButtonHidden(true);
+  };
+
+  const openFormHandler = () => {
+    setFormExpanded(true);
+    setButtonHidden(false);
+  };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEneteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-      </div>
-
-      <div className="new-expense__control">
-        <label>Amount</label>
-        <input
-          type="number"
-          min="0.01"
-          step="0.01"
-          value={enteredAmount}
-          onChange={amountChangeHandler}
-        />
-      </div>
-
-      <div className="new-expense__control">
-        <label>Date</label>
-        <input
-          type="date"
-          min="2019-01-01"
-          max="2022-12-31"
-          value={enteredDate}
-          onChange={dateChangeHandler}
-        />
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    <div>
+      {formExpanded ? (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button" onClick={closeFormHandler}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      ) : (
+        <button type="button" onClick={openFormHandler}>
+          {buttonHidden ? "Add New Expense" : "+ New Expense"}
+        </button>
+      )}
+    </div>
   );
 };
 
